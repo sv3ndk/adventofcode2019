22b0524cf373b28dc1fd252c7c9111b75eb95961
sv3ndk <svend.vanderveken@gmail.com>
2019-12-07 18:37:40 +0100

    adds solution for day2 part 2

--- src/main/scala/advent/Day2.scala
+++ src/main/scala/advent/Day2.scala
@@ -3,14 +3,13 @@ package advent
 import scala.annotation.tailrec
 
 object Day2 extends App {
-
   println("Advent of code 2019 - Day 2")
 
-//  val program = Seq(1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,2,9,19,23,1,9,23,27,2,27,9,31,1,31,5,35,2,35,9,39,1,39,10,43,2,43,13,47,1,47,6,51,2,51,10,55,1,9,55,59,2,6,59,63,1,63,6,67,1,67,10,71,1,71,10,75,2,9,75,79,1,5,79,83,2,9,83,87,1,87,9,91,2,91,13,95,1,95,9,99,1,99,6,103,2,103,6,107,1,107,5,111,1,13,111,115,2,115,6,119,1,119,5,123,1,2,123,127,1,6,127,0,99,2,14,0,0)
-  val program = Seq(1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,2,9,19,23,1,9,23,27,2,27,9,31,1,31,5,35,2,35,9,39,1,39,10,43,2,43,13,47,1,47,6,51,2,51,10,55,1,9,55,59,2,6,59,63,1,63,6,67,1,67,10,71,1,71,10,75,2,9,75,79,1,5,79,83,2,9,83,87,1,87,9,91,2,91,13,95,1,95,9,99,1,99,6,103,2,103,6,107,1,107,5,111,1,13,111,115,2,115,6,119,1,119,5,123,1,2,123,127,1,6,127,0,99,2,14,0,0)
+  val resultPart1 = Day2Solution.run(12, 2)
+  println(s"Part 1: program after execution: ${resultPart1}")           // 9581917
 
-  val result = Day2Solution.run(0, program)
-  println(s"program after execution: ${result}")
+  val resultPart2 = Day2Solution.inputFor(19690720)
+  println(s"Part 2: input noun and verb for 19690720: $resultPart2")    // (25, 5)
 
 }
 
@@ -18,8 +17,8 @@ object Day2Solution {
 
   type Program = Seq[Int]
 
-  def operation(position: Int, program: Program, op: (Int, Int) => Int): Program = {
-    val _ :: offset1 :: offset2 :: resultOffset :: Nil = program.slice(position, position + 4)
+  def operation(pointer: Int, program: Program, op: (Int, Int) => Int): Program = {
+    val _ :: offset1 :: offset2 :: resultOffset :: Nil = program.slice(pointer, pointer + 4)
     val updated = op(program(offset1), program(offset2))
     program.take(resultOffset) :+ updated :++ program.drop(resultOffset + 1)
   }
@@ -28,23 +27,45 @@ object Day2Solution {
   val multiplier = operation(_, _, (a, b) => a * b)
 
   @tailrec
-  def run(position: Int, program: Program): Program = {
-    assert(position >= 0 && position < program.length)
-    val operation = program(position)
+  def runProgram(pointer: Int, program: Program): Program = {
+    assert(pointer >= 0 && pointer < program.length)
+    val operation = program(pointer)
 
-    println(s"operation ${operation} at position ${position}, program ${program}")
+//    println(s"operation ${operation} at position ${pointer}, program ${program}")
 
     if (operation == 99) {
       program
     } else {
 
-      val newProgram = program(position) match {
-        case 1 => adder(position, program)
-        case 2 => multiplier(position, program)
-        case other => throw new RuntimeException(s"unknown operation: ${other} at position ${position} for ${program}")
+      val newProgram = program(pointer) match {
+        case 1 => adder(pointer, program)
+        case 2 => multiplier(pointer, program)
+        case other => throw new RuntimeException(s"unknown operation: ${other} at position ${pointer} for ${program}")
       }
 
-      run(position + 4, newProgram)
+      runProgram(pointer + 4, newProgram)
     }
   }
+
+  // execute the program of this exercise, with input parameters
+  def run(noun: Int, verb: Int): Int = {
+//    println(s" running for $noun, $verb")
+    val program = 1 :: noun :: verb :: List(3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,2,9,19,23,1,9,23,27,2,27,9,31,1,31,5,35,2,35,9,39,1,39,10,43,2,43,13,47,1,47,6,51,2,51,10,55,1,9,55,59,2,6,59,63,1,63,6,67,1,67,10,71,1,71,10,75,2,9,75,79,1,5,79,83,2,9,83,87,1,87,9,91,2,91,13,95,1,95,9,99,1,99,6,103,2,103,6,107,1,107,5,111,1,13,111,115,2,115,6,119,1,119,5,123,1,2,123,127,1,6,127,0,99,2,14,0,0)
+    val updatedProgram = runProgram(0, program)
+    updatedProgram.head
+  }
+
+  // executes the program until it finds input that yields the specified input
+  def inputFor(expectedOutput: Int): (Int, Int) = {
+
+    val (noun, verb, result) = LazyList
+      .tabulate(100, 100) { (noun, verb) => (noun, verb, run(noun,verb))}
+      .flatten
+      .filter{ case (noun, verb, result) => result == expectedOutput }
+      .head
+
+    (noun, verb)
+
+  }
+
 }
--- src/test/scala/advent/Day2Spec.scala
+++ src/test/scala/advent/Day2Spec.scala
@@ -17,19 +17,23 @@ class Day2Spec extends FlatSpec with Matchers {
   }
 
   "example program" should "match" in {
-    Day2Solution.run(0, Seq (1,0,0,0,99)) should be (Seq(2,0,0,0,99))
+    Day2Solution.runProgram(0, Seq (1,0,0,0,99)) should be (Seq(2,0,0,0,99))
   }
 
   "example program2" should "match" in {
-    Day2Solution.run(0, Seq (2,3,0,3,99)) should be (Seq(2,3,0,6,99))
+    Day2Solution.runProgram(0, Seq (2,3,0,3,99)) should be (Seq(2,3,0,6,99))
   }
 
   "example program3" should "match" in {
-    Day2Solution.run(0, Seq(2,4,4,5,99,0)) should be (Seq(2,4,4,5,99,9801))
+    Day2Solution.runProgram(0, Seq(2,4,4,5,99,0)) should be (Seq(2,4,4,5,99,9801))
   }
 
   "example program4" should "match" in {
-    Day2Solution.run(0, Seq (1,1,1,4,99,5,6,0,99)) should be (Seq(30,1,1,4,2,5,6,0,99))
+    Day2Solution.runProgram(0, Seq (1,1,1,4,99,5,6,0,99)) should be (Seq(30,1,1,4,2,5,6,0,99))
+  }
+
+  "running part 2 solution with part 1 input" should " yield the same result" in {
+    Day2Solution.run(12, 2) should be (9581917)
   }
 
 }
