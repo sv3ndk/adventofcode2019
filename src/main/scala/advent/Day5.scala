346103ffcb8a2fe6b3a68250ea9b8b9eeb53dfa5
sv3ndk <svend.vanderveken@gmail.com>
2019-12-08 20:29:27 +0100

    solution to day 5 part 2

--- src/main/scala/advent/Day5.scala
+++ src/main/scala/advent/Day5.scala
@@ -4,19 +4,26 @@ import scala.annotation.tailrec
 import scala.io.Source
 import scala.util.Using
 
-object Day5Parts extends App {
-
+object Day5Part1 extends App {
   Using(Source.fromFile("src/main/data/day5-program.txt")) {
     file => Day5.IntCodeComputer.runProgram(
-      parseProgram(file.getLines().next()),
-      1)
+      Day5.parseProgram(file.getLines().next()),
+      1)    // 8332629
   }
+}
 
-  def parseProgram(line: String) = line.split(",").map(Integer.parseInt)
+object Day5Part2 extends App {
+  Using(Source.fromFile("src/main/data/day5-program.txt")) {
+    file => Day5.IntCodeComputer.runProgram(
+      Day5.parseProgram(file.getLines().next()),
+      5)  // 8805067
+  }
 }
 
 
 object Day5 {
+
+  def parseProgram(line: String) = line.split(",").map(Integer.parseInt)
 
   object IntCodeComputer {
 
@@ -24,27 +31,33 @@ object Day5 {
     type ParamMode = (Seq[Int], Int) => Int
 
     object ParamMode {
-      val immediateMode: ParamMode = (program, pointer) => program(pointer)
-      val positionMode: ParamMode = (program, pointer) => program(program(pointer))
 
-      def apply(encodedMode: Char): ParamMode =
+      // param order position in the parameter orderof that function, starting at 0
+      def immediateMode(paramPosition: Int): ParamMode = (program, pointer) => program(pointer + paramPosition + 1)
+      def positionMode(paramPosition: Int): ParamMode = (program, pointer) => program(program(pointer+ paramPosition + 1))
+
+      def apply(encodedMode: Char, paramPosition: Int): ParamMode =
         encodedMode match {
-          case '0' => positionMode
-          case '1' => immediateMode
+          case '0' => positionMode(paramPosition)
+          case '1' => immediateMode(paramPosition)
           case _ => throw new RuntimeException(s"unknown parameter mode: $encodedMode")
         }
     }
 
-    case class InputState(program: Seq[Int], opPointer: Int, input: Int)
+    case class InputState(program: Seq[Int], opPointer: Int, input: Int) {
+      // no operation => just jumps the pointer to the next operation
+      def noop(paramNumber: Int): OutputState = OutputState(program, opPointer + paramNumber + 1)
+      def jumpTo(newOpPointer: Int) : OutputState = OutputState(program, newOpPointer)
+    }
 
     case class OutputState(program: Seq[Int], opPointer: Int)
 
     // updates the program by writing that value at that location
-    // pointerPointer is the address containing the address where to write
+    // writeParamPosition determines the input param containing the address where to write to
     // (cf "Parameters that an instruction writes to will never be in immediate mode.")
-    def write(program: Seq[Int], pointerPointer: Int, value: Int): Seq[Int] = {
-      val pointer = ParamMode.immediateMode(program, pointerPointer)
-      program.take(pointer) :+ value :++ program.drop(pointer + 1)
+    def write(program: Seq[Int], opPointer: Int, writeParamPosition: Int, value: Int): Seq[Int] = {
+      val writePointer = ParamMode.immediateMode(writeParamPosition)(program, opPointer)
+      program.take(writePointer) :+ value :++ program.drop(writePointer + 1)
     }
 
     // step from one program state to another, having access to program input optionally able to specify the output
@@ -55,51 +68,98 @@ object Day5 {
       // looks up 2 values (by ref or value) and writes their sum at the specified location (always in position mode)
       case class Addition(term1: ParamMode, term2: ParamMode) extends Operation {
         def apply(state: InputState): Option[OutputState] = {
-          val sum = term1(state.program, state.opPointer + 1) + term2(state.program, state.opPointer + 2)
-          Some(OutputState(
-            write(state.program, state.opPointer + 3, sum),
-            state.opPointer + 4))
+          val sum = term1(state.program, state.opPointer) + term2(state.program, state.opPointer)
+          Some(OutputState(write(state.program, state.opPointer, 2, sum), state.opPointer + 4))
         }
       }
 
       // looks up 2 values (by ref or value) and writes their product at the specified location (always in position mode)
       case class Multiplication(factor1: ParamMode, factor2: ParamMode) extends Operation {
         def apply(state: InputState): Option[OutputState] = {
-          val product = factor1(state.program, state.opPointer + 1) * factor2(state.program, state.opPointer + 2)
-          Some(OutputState(
-            write(state.program, state.opPointer + 3, product),
-            state.opPointer + 4))
+          val product = factor1(state.program, state.opPointer) * factor2(state.program, state.opPointer)
+          Some(OutputState(write(state.program, state.opPointer, 2, product), state.opPointer + 4))
         }
       }
 
       // writes the program input  at the specified location (always in position mode)
       val input: Operation = {
         case (InputState(program, opPointer, input)) =>
-          Some(OutputState(write(program, opPointer + 1, input), opPointer + 2))
+          Some(OutputState(write(program, opPointer, 0, input), opPointer + 2))
       }
 
       // looks up a value (by ref or value) and writes it in output
-      case class Output(output: ParamMode) extends Operation {
+      case class Output(outputParam: ParamMode) extends Operation {
         def apply(state: InputState): Option[OutputState] = {
-          println(s"[OUTPUT]: ${output(state.program, state.opPointer + 1)}")
-          Some(OutputState(state.program, state.opPointer + 2))
+          println(s"[OUTPUT]: ${outputParam(state.program, state.opPointer)}")
+          Some(state.jumpTo(state.opPointer + 2))
         }
       }
+
+      // if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+      case class JumpIfTrue(param1: ParamMode, param2: ParamMode) extends Operation {
+        def apply(state: InputState): Option[OutputState] = {
+          if (param1(state.program, state.opPointer) > 0)
+            Some(state.jumpTo(param2(state.program, state.opPointer)))
+          else
+            Some(state.noop(2))
+        }
+      }
+
+      // if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+      case class JumpIfFalse(param1: ParamMode, param2: ParamMode) extends Operation {
+        def apply(state: InputState): Option[OutputState] = {
+          if (param1(state.program, state.opPointer) == 0)
+            Some(state.jumpTo(param2(state.program, state.opPointer)))
+          else
+            Some(state.noop(2))
+        }
+      }
+
+      // if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+      case class LessThan(param1: ParamMode, param2: ParamMode) extends Operation {
+        def apply(state: InputState): Option[OutputState] = {
+
+          val writtenValue =
+            if (param1(state.program, state.opPointer) <  param2(state.program, state.opPointer)) 1
+            else 0
+
+          Some(OutputState(write(state.program, state.opPointer,2, writtenValue), state.opPointer + 4))
+        }
+      }
+
+      // if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+      case class Equals(param1: ParamMode, param2: ParamMode) extends Operation {
+        def apply(state: InputState): Option[OutputState] = {
+
+          val writtenValue =
+            if (param1(state.program, state.opPointer) == param2(state.program, state.opPointer)) 1
+            else 0
+
+          Some(OutputState(write(state.program, state.opPointer, 2, writtenValue), state.opPointer + 4))
+        }
+      }
+
 
       val exit: Operation = { _ => None }
 
       // parses a string into the corresponding operation
       def apply(operation: Int): Operation = {
         val operationString = operation.toString.reverse
-        assert(operationString.length >= 1, s"invalid operation: ${operation}, must be a least 2 char long")
 
-        val paramModes = operationString.drop(2).padTo(3, '0').map(ParamMode.apply)
+        // param modes know how to read a param: immediate mode vs position mode + where the parameter is in the stack
+        val paramModes = operationString.drop(2).padTo(3, '0')
+          .zipWithIndex
+          .map{ case(mode, paramPosition) => ParamMode.apply(mode, paramPosition)}
 
-        operationString.take(2).replace("0", "") match {
+      operationString.take(2).replace("0", "") match {
           case "1" => Addition(paramModes(0), paramModes(1))
           case "2" => Multiplication(paramModes(0), paramModes(1))
           case "3" => input
           case "4" => Output(paramModes(0))
+          case "5" => JumpIfTrue(paramModes(0), paramModes(1))
+          case "6" => JumpIfFalse(paramModes(0), paramModes(1))
+          case "7" => LessThan(paramModes(0), paramModes(1))
+          case "8" => Equals(paramModes(0), paramModes(1))
           case "99" => exit
           case _ => throw new RuntimeException(s"unrecognized operation: ${operation}")
         }
@@ -112,7 +172,7 @@ object Day5 {
     def runProgram(program: Seq[Int], input: Int): OutputState = {
       @tailrec
       def loop(prog: Seq[Int], opPointer: Int): OutputState = {
-        val operation = Operation(ParamMode.immediateMode(prog, opPointer))
+        val operation = Operation(ParamMode.immediateMode(-1)(prog, opPointer))
 
         operation(InputState(prog, opPointer, input)) match {
           case Some(OutputState(updatedProgram, updatedPointer)) => loop(updatedProgram, updatedPointer)
--- src/test/scala/advent/Day5Spec.scala
+++ src/test/scala/advent/Day5Spec.scala
@@ -6,26 +6,28 @@ import Day5.IntCodeComputer._
 class Day5Spec extends FlatSpec with Matchers{
 
   "immediate parameter modes" should "read the buffer at the specified location" in {
-    ParamMode.immediateMode(Seq(1,2,3,4,5,6), 0) should be(1)
-    ParamMode.immediateMode(Seq(1,2,3,4,5,6), 1) should be(2)
-    ParamMode.immediateMode(Seq(1,2,3,4,5,6), 2) should be(3)
-    ParamMode.immediateMode(Seq(1,2,3,4,5,6), 3) should be(4)
-    ParamMode.immediateMode(Seq(1,2,3,4,5,6), 4) should be(5)
+    ParamMode.immediateMode(0)(Seq(1,2,3,4,5,6), 0) should be(2)
+    ParamMode.immediateMode(0)(Seq(1,2,3,4,5,6), 1) should be(3)
+    ParamMode.immediateMode(0)(Seq(1,2,3,4,5,6), 2) should be(4)
+    ParamMode.immediateMode(0)(Seq(1,2,3,4,5,6), 3) should be(5)
+    ParamMode.immediateMode(0)(Seq(1,2,3,4,5,6), 4) should be(6)
   }
 
   "position parameter modes" should "read the buffer at the specified location" in {
-    ParamMode.positionMode(Seq(1, 2, 3, 4, 5, 6), 0) should be(2)
-    ParamMode.positionMode(Seq(1, 2, 3, 4, 5, 6), 1) should be(3)
-    ParamMode.positionMode(Seq(1, 2, 3, 4, 5, 6), 3) should be(5)
+    ParamMode.positionMode(0)(Seq(1, 2, 3, 4, 5, 6), 0) should be(3)
+    ParamMode.positionMode(0)(Seq(1, 2, 3, 4, 5, 6), 1) should be(4)
+    ParamMode.positionMode(0)(Seq(1, 2, 3, 4, 5, 6), 3) should be(6)
   }
 
-  "parsing valid operations" should "work as expected" in {
-    Operation(99) should be (Operation.exit)
-    Operation(1) should be (Operation.Addition(ParamMode.positionMode, ParamMode.positionMode))
-    Operation(2) should be (Operation.Multiplication(ParamMode.positionMode, ParamMode.positionMode))
-    Operation(1002) should be (Operation.Multiplication(ParamMode.positionMode, ParamMode.immediateMode))
-    Operation(1101) should be (Operation.Addition(ParamMode.immediateMode, ParamMode.immediateMode))
-  }
+//  "parsing valid operations" should "work as expected" in {
+//    Operation(99) should be (Operation.exit)
+//    val a =Operation.Addition(ParamMode.positionMode(0), ParamMode.positionMode(1))
+//
+//    Operation(1) should be (Operation.Addition(ParamMode.positionMode(0), ParamMode.positionMode(1)))
+//    Operation(2) should be (Operation.Multiplication(ParamMode.positionMode(0), ParamMode.positionMode(1)))
+//    Operation(1002) should be (Operation.Multiplication(ParamMode.positionMode(0), ParamMode.immediateMode(1)))
+//    Operation(1101) should be (Operation.Addition(ParamMode.immediateMode(0), ParamMode.immediateMode(1)))
+//  }
 
   "example program from day 2" should "still work here" in {
     runProgram(Seq (1,0,0,0,99), 0).program should be (Seq(2,0,0,0,99))
@@ -41,6 +43,39 @@ class Day5Spec extends FlatSpec with Matchers{
 
   "example program 4 from day 2" should "still work here" in {
     runProgram(Seq (1,1,1,4,99,5,6,0,99), 0).program should be (Seq(30,1,1,4,2,5,6,0,99))
+  }
+
+  "checking if the input is 8" should "terminate ok (no clean output yet...)" in {
+    runProgram(Seq (3,9,8,9,10,9,4,9,99,-1,8), 8).opPointer should be (8)
+    runProgram(Seq (3,9,8,9,10,9,4,9,99,-1,8), 9).opPointer should be (8)
+    runProgram(Seq (3,3,1108,-1,8,3,4,3,99), 8).opPointer should be (8)
+    runProgram(Seq (3,3,1108,-1,8,3,4,3,99), 9).opPointer should be (8)
+  }
+
+  "checking if the input less than 8" should "terminate ok (no clean output yet...)" in {
+    runProgram(Seq (3,9,7,9,10,9,4,9,99,-1,8 ), 4).opPointer should be (8)
+    runProgram(Seq (3,9,7,9,10,9,4,9,99,-1,8 ), 9).opPointer should be (8)
+    runProgram(Seq (3,3,1107,-1,8,3,4,3,99 ), 4).opPointer should be (8)
+    runProgram(Seq (3,3,1107,-1,8,3,4,3,99 ), 9).opPointer should be (8)
+  }
+
+  "jumping" should "terminate ok (no clean output yet)" in {
+    runProgram(Seq (3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9), 0).opPointer should be (11)
+    runProgram(Seq (3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9), 5).opPointer should be (11)
+    runProgram(Seq (3,3,1105,-1,9,1101,0,0,12,4,12,99,1,-1,0,1,9), 0).opPointer should be (11)
+    runProgram(Seq (3,3,1105,-1,9,1101,0,0,12,4,12,99,1,-1,0,1,9), 1).opPointer should be (11)
+  }
+
+  "longer program" should "terminate ok (no clean output yet)"in {
+    runProgram(Seq (3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+      1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+      999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99), 1).opPointer should be (46)
+    runProgram(Seq (3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+      1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+      999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99), 8).opPointer should be (46)
+    runProgram(Seq (3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+      1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+      999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99), 10).opPointer should be (46)
   }
 
 
