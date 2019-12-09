1f45e3455c37efff0e01ced03fb5630630669e53
sv3ndk <svend.vanderveken@gmail.com>
2019-12-10 00:12:46 +0100

    solution to day 7 part 2

--- src/main/scala/advent/Day7.scala
+++ src/main/scala/advent/Day7.scala
@@ -1,27 +1,84 @@
 package advent
 
+import advent.Day7.IntCodeComputer.State
 
 import scala.annotation.tailrec
 import scala.io.Source
 import scala.util.Using
 
-object Day7Part extends App {
+/**
+ * Solution to day7 part 1
+ *
+ * In this settings, we assume that the program never pauses => all inputs are always available at start.
+ * We also assumes that each execution is only returning 1 parameter
+ *
+ */
+object Day7Part1 extends App {
 
-  val computer = Day7.IntCodeComputer.parseProgram("src/main/data/day7-amplifier-program.txt")
-  val (phaseSettings, power)  = optimalPhases(computer)
-  println(s"Advent of code 2019 - Day 7 / part 1: $power with settingsr $phaseSettings")
+  val (phaseSettings, power) = optimalPhases(Day7.day7IntCodeComputer)
+  println(s"Advent of code 2019 - Day 7 / part 1: $power with settings $phaseSettings")
 
   // runs that program as many times as there are phase settings, chaining each output to as input for the next amp
-  def chainAmplifiers(amplifierControlSoftware: Seq[Int], phaseSettings: Seq[Int], firstInput: Int): Int = {
-    val intComputer = new Day7.IntCodeComputer(amplifierControlSoftware)
-    phaseSettings.foldLeft(firstInput) { (output, phase) => intComputer.run(phase, output).head }
+  def chainAmplifiers(amplifierControlSoftware: Seq[Int], phaseSettings: Seq[Int]): Int = {
+    val intCodeComputer = new Day7.IntCodeComputer(amplifierControlSoftware)
+    phaseSettings.foldLeft(0) { (prevOutput, phase) => intCodeComputer.start(Seq(phase, prevOutput)).output.head }
   }
 
   // finds the set of phase settings that yield the best thrust power
   def optimalPhases(amplifierControlSoftware: Seq[Int]): (Seq[Int], Int) = {
     List(0, 1, 2, 3, 4)
       .permutations
-      .map(phaseSettings => (phaseSettings, chainAmplifiers(amplifierControlSoftware, phaseSettings, 0)))
+      .map(phaseSettings => (phaseSettings, chainAmplifiers(amplifierControlSoftware, phaseSettings)))
+      .reduce((run1, run2) => if (run1._2 > run2._2) run1 else run2)
+  }
+}
+
+
+/**
+ * Solution to day7 part 2
+ *
+ * in this setting, we assume that programs are pausing when not enough input is available
+ */
+object Day7Part2 extends App {
+
+  val (phaseSettings, power) = optimalPhases(Day7.day7IntCodeComputer)
+  println(s"Advent of code 2019 - Day 7 / part 2: $power with settings $phaseSettings")
+
+  // runs the amplifiers in loop, chain them into each other then repeating until one of them is stopped
+  def loopingAmplifiersChain(amplifierControlSoftware: Seq[Int], phaseSettings: Seq[Int]): Int = {
+    val intCodeComputer = new Day7.IntCodeComputer(amplifierControlSoftware)
+
+    val Seq(phaseA, phaseB, phaseC, phaseD, phaseE) = phaseSettings
+
+    // initial execution of each program, using the phases as input
+    val pausedStateA = intCodeComputer.start(Seq(phaseA, 0), "A")
+    val pausedStateB = intCodeComputer.start(phaseB +: pausedStateA.output, "B")
+    val pausedStateC = intCodeComputer.start(phaseC +: pausedStateB.output, "C")
+    val pausedStateD = intCodeComputer.start(phaseD +: pausedStateC.output, "D")
+    val pausedStateE = intCodeComputer.start(phaseE +: pausedStateD.output, "E")
+
+    @tailrec
+    def runAmp(ampStates: Seq[State]): Int = {
+
+      if (ampStates.exists(_.isStopped))
+        ampStates.last.output.head  // this assumes a stopped program always returns one single output
+      else {
+        val executedA = intCodeComputer.resume(ampStates.head, ampStates.last.output)
+        val newStates = ampStates.tail.foldLeft(Seq(executedA)) {
+          (executedStates, state) => {intCodeComputer.resume(state, executedStates.head.output) +: executedStates}
+        }
+        runAmp(newStates.reverse)
+      }
+    }
+
+    runAmp(Seq(pausedStateA, pausedStateB, pausedStateC, pausedStateD, pausedStateE))
+  }
+
+  // finds the set of phase settings that yield the best thrust power
+  def optimalPhases(amplifierControlSoftware: Seq[Int]): (Seq[Int], Int) = {
+    List(5,6,7,8,9)
+      .permutations
+      .map(phaseSettings => (phaseSettings, loopingAmplifiersChain(amplifierControlSoftware, phaseSettings)))
       .reduce((run1, run2) => if (run1._2 > run2._2) run1 else run2)
   }
 
@@ -30,43 +87,50 @@ object Day7Part extends App {
 
 object Day7 {
 
-  import advent.Day7.IntCodeComputer.State
+  val day7IntCodeComputer =
+    Using(Source.fromFile("src/main/data/day7-amplifier-program.txt")) { file => file.getLines().toSeq.head }
+      .get
+      .split(",").map(Integer.parseInt)
+      .toSeq
 
   /**
-   * 3rd iteration on the initCode computer:
+   * 3rd iteration on the intCode computer:
    *
-   * this one accepts a sequence of inputs and provides a Seq of outputs. When an input is read by the program, it is
-   * un-piled from the stack and no longer available
+   * This one accepts a sequence of inputs and provides a Seq of outputs.
+   *
+   * When an input is read by the program, it is un-piled from the stack and no longer available.
+   *
+   * If no input is available during a read operation, the program pause, and can be resumed later,
+   *
    */
   class IntCodeComputer(startProgram: Seq[Int]) {
 
     /**
-     * Main entry point of this IntCode computer: execute this program with this input and provides the output as
-     * return value
+     * Main entry point of this IntCode computer: execute this program with this input and provides the resulting state
+     * as return value
      */
-    def run(input: Int*): Seq[Int] = {
-      //      println(s"running with $input")
-      doRun(input: _*).output
+    def start(inputs: Seq[Int], label: String = "noname"): State = loop(new State(startProgram, inputs, label))
+
+    /**
+     * Resumes the execution of this program, keeping the paused programme and pointer but resetting its i/o
+     */
+    def resume(state: State, input: Seq[Int]): State = {
+      assert(state.isPaused, "inconsistent state: cannot resume a non paused program")
+      loop(state.unpause(input))
     }
 
-    // verbose output version, for UTs
-    def doRun(input: Int*): State = loop(new State(startProgram, input.toList))
+    // convenience executor, when there is only one input (backward compatibility with old tests)
+    def start(input: Int): State = start(Seq(input))
 
-    @tailrec private def loop(state: State): State = if (!state.isRunning) state else loop(state.step)
+    @tailrec private def loop(state: State): State = if (state.isRunning) loop(state.step) else state
   }
 
   object IntCodeComputer {
 
-    def parseProgram(programLocation: String): Seq[Int] = {
-      Using(Source.fromFile(programLocation)) { file => file.getLines().toSeq.head }
-        .get
-        .split(",").map(Integer.parseInt)
-        .toSeq
-    }
+    case class State(program: Seq[Int], opPointer: Int, label: String,
+                     input: Seq[Int], output: Seq[Int], isStopped: Boolean, isPaused: Boolean) {
 
-    case class State(program: Seq[Int], opPointer: Int, input: Seq[Int], output: Seq[Int], isRunning: Boolean) {
-
-      def this(program: Seq[Int], input: Seq[Int]) = this(program, 0, input, Nil, true)
+      def this(program: Seq[Int], input: Seq[Int], label: String = "noname") = this(program, 0, label, input, Nil, false, false)
 
       // move the operation pointer to the specified location
       def jumpTo(newOpPointer: Int): State = copy(opPointer = newOpPointer)
@@ -75,9 +139,14 @@ object Day7 {
       def forward(size: Int): State = jumpTo(opPointer + size)
 
       // adds this to the program output
-      def addOutput(moreOutPut: Int) = copy(output = moreOutPut +: output)
+      def addOutput(moreOutPut: Int): State = copy(output = moreOutPut +: output)
 
-      lazy val stopped = copy(isRunning = false)
+      lazy val stopped = copy(isStopped = true)
+      lazy val paused = copy(isPaused = true)
+      lazy val isRunning = !isStopped && !isPaused
+
+      // un-pause this program, using those new inputs
+      def unpause(input: Seq[Int]): State = copy(isPaused = false, input = input, output = Nil)
 
       lazy val currentOpCode: Int = program(opPointer)
 
@@ -92,11 +161,9 @@ object Day7 {
         copy(program = program.take(writePointer) :+ value :++ program.drop(writePointer + 1))
       }
 
-      // reads one input from the program (which is then removed and no longer available later)
-      lazy val readInput = {
-        assert(input.nonEmpty, "cannot access input: Empty (not enough argument?)")
-        (input.head, copy(input = input.tail))
-      }
+      // tries to pop one input, if available
+      lazy val readInput: Option[(Int, State)] =
+        input.headOption.map(existingInput => (existingInput, copy(input = input.tail)))
     }
 
     // accessor of a function parameter in the program
@@ -105,13 +172,10 @@ object Day7 {
     object Param {
 
       // param order: position among the parameters of the function this parameter belongs to, starting at 0
-      def immediateMode(paramPosition: Int): Param = {
-        case State(program, pointer, _, _, _) => program(pointer + paramPosition + 1)
-      }
+      def immediateMode(paramPosition: Int): Param = state => state.program(state.opPointer + paramPosition + 1)
 
-      def positionMode(paramPosition: Int): Param = {
-        case State(program, pointer, _, _, _) => program(program(pointer + paramPosition + 1))
-      }
+      def positionMode(paramPosition: Int): Param =
+        state => state.program(state.program(state.opPointer + paramPosition + 1))
 
       def apply(encodedMode: Char, paramPosition: Int): Param =
         encodedMode match {
@@ -126,7 +190,7 @@ object Day7 {
 
     object Operation {
 
-      // looks up 2 values and writes their sum at the specified location
+      //  looks up 2 values and writes their sum at the specified location
       def addition(val1: Param, val2: Param): Operation =
         state => state.write(2, val1(state) + val2(state)).forward(4)
 
@@ -134,11 +198,13 @@ object Day7 {
       def multiplication(val1: Param, val2: Param): Operation =
         state => state.write(2, val1(state) * val2(state)).forward(4)
 
-      // consumes one input and writes it in the program at the specified location
-      val input: Operation = (state: State) => {
-        val (consumedInput, updatedState) = state.readInput
-        updatedState.write(0, consumedInput).forward(2)
-      }
+      // consumes one input and writes it in the program at the specified location, or pause if no input available
+      val input: Operation = (state: State) =>
+        state.readInput match {
+          case Some((consumedInput, updatedState)) =>
+            updatedState.write(0, consumedInput).forward(2)
+          case None => state.paused
+        }
 
       // looks up a value (by ref or value) and writes it in output
       def output(param: Param): Operation = state => state.addOutput(param(state)).forward(2)
@@ -157,7 +223,7 @@ object Day7 {
 
       val exit: Operation = (state: State) => state.stopped
 
-      // determines the Operation c
+      // determines the Operation corresponding to that intCode
       def apply(operation: Int): Operation = {
 
         val operationStringRev = operation.toString.reverse
--- src/test/scala/advent/Day7PartSpec.scala
+++ src/test/scala/advent/Day7PartSpec.scala
@@ -7,7 +7,7 @@ import advent.Day7.IntCodeComputer
 class Day7PartSpec extends FlatSpec with Matchers {
 
   "(from day5) immediate parameter modes" should "read the buffer at the specified location" in {
-    val prog = new State(Seq(1,2,3,4,5,6),List(12345))
+    val prog = new State(Seq(1,2,3,4,5,6),List(12345), "rst")
 
     Param.immediateMode(0)(prog) should be(2)
     Param.immediateMode(1)(prog) should be(3)
@@ -18,70 +18,70 @@ class Day7PartSpec extends FlatSpec with Matchers {
   }
 
   "(from day5) position parameter modes" should "read the buffer at the specified location" in {
-    val prog = new State(Seq(1,2,3,4,5,6),List(12345))
+    val prog = new State(Seq(1,2,3,4,5,6),List(12345), "")
     Param.positionMode(0)(prog) should be(3)
     Param.positionMode(1)(prog) should be(4)
     Param.positionMode(2)(prog) should be(5)
   }
 
   "(from day5) example program from day 2" should "still work here" in {
-    new IntCodeComputer(Seq (1,0,0,0,99)).doRun( 0).program should be (Seq(2,0,0,0,99))
+    new IntCodeComputer(Seq (1,0,0,0,99)).start( List(0)).program should be (Seq(2,0,0,0,99))
   }
 
   "(from day5) example program 2 from day 2" should "still work here" in {
-    new IntCodeComputer(Seq (2,3,0,3,99)).doRun( 0).program should be (Seq(2,3,0,6,99))
+    new IntCodeComputer(Seq (2,3,0,3,99)).start( List(0)).program should be (Seq(2,3,0,6,99))
   }
 
   "(from day5) example program 3 from day 2" should "still work here" in {
-    new IntCodeComputer(Seq(2,4,4,5,99,0)).doRun( 0).program should be (Seq(2,4,4,5,99,9801))
+    new IntCodeComputer(Seq(2,4,4,5,99,0)).start( List(0)).program should be (Seq(2,4,4,5,99,9801))
   }
 
   "(from day5) example program 4 from day 2" should "still work here" in {
-    new IntCodeComputer(Seq (1,1,1,4,99,5,6,0,99)).doRun( 0).program should be (Seq(30,1,1,4,2,5,6,0,99))
+    new IntCodeComputer(Seq (1,1,1,4,99,5,6,0,99)).start( List(0)).program should be (Seq(30,1,1,4,2,5,6,0,99))
   }
 
 
   "(from day5) consider whether the input is equal to 8" should "output 1 (if it is) or 0 (if it is not)." in {
     val program = Seq(3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8)
-    new IntCodeComputer(program).run( 8) should be (Seq(1))
-    new IntCodeComputer(program).run( 3) should be (Seq(0))
-    new IntCodeComputer(program).run( 13) should be (Seq(0))
+    new IntCodeComputer(program).start( 8).output should be (Seq(1))
+    new IntCodeComputer(program).start( 3).output should be (Seq(0))
+    new IntCodeComputer(program).start( 13).output should be (Seq(0))
   }
 
   "(from day5) consider whether the input is less than 8" should "output 1 (if it is) or 0 (if it is not)." in {
     val program = Seq(3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8)
-    new IntCodeComputer(program).run( 1) should be (Seq(1))
-    new IntCodeComputer(program).run( 3) should be (Seq(1))
-    new IntCodeComputer(program).run( 8) should be (Seq(0))
-    new IntCodeComputer(program).run( 18) should be (Seq(0))
+    new IntCodeComputer(program).start( 1).output should be (Seq(1))
+    new IntCodeComputer(program).start( 3).output should be (Seq(1))
+    new IntCodeComputer(program).start( 8).output should be (Seq(0))
+    new IntCodeComputer(program).start( 18).output should be (Seq(0))
   }
 
   "(from day5) Using immediate mode, consider whether the input is equal to 8" should "output 1 (if it is) or 0 (if it is not)." in {
     val program = Seq(3,3,1108,-1,8,3,4,3,99)
-    new IntCodeComputer(program).run( 1) should be(Seq(0))
-    new IntCodeComputer(program).run( 8) should be(Seq(1))
-    new IntCodeComputer(program).run( 10) should be(Seq(0))
+    new IntCodeComputer(program).start( 1).output should be(Seq(0))
+    new IntCodeComputer(program).start( 8).output should be(Seq(1))
+    new IntCodeComputer(program).start( 10).output should be(Seq(0))
   }
 
   "(from day5) Using immediate mode, consider whether the input is less than 8" should "output 1 (if it is) or 0 (if it is not)." in {
     val program = Seq(3,3,1107,-1,8,3,4,3,99)
-    new IntCodeComputer(program).run(8) should be(Seq(0))
-    new IntCodeComputer(program).run( 1) should be(Seq(1))
-    new IntCodeComputer(program).run(8) should be(Seq(0))
+    new IntCodeComputer(program).start(8).output should be(Seq(0))
+    new IntCodeComputer(program).start( 1).output should be(Seq(1))
+    new IntCodeComputer(program).start(8).output should be(Seq(0))
   }
 
   "(from day5) using position mode, some jump tests that take an input, then" should "output 0 if the input was zero or 1 if the input was non-zero:" in {
     val program = Seq(3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9)
-    new IntCodeComputer(program).run( -10) should be (Seq(1))
-    new IntCodeComputer(program).run( 0) should be (Seq(0))
-    new IntCodeComputer(program).run( 10) should be (Seq(1))
+    new IntCodeComputer(program).start( -10).output should be (Seq(1))
+    new IntCodeComputer(program).start( 0).output should be (Seq(0))
+    new IntCodeComputer(program).start( 10).output should be (Seq(1))
   }
 
   "(from day5) using immediate mode, some jump tests that take an input, then" should "output 0 if the input was zero or 1 if the input was non-zero:" in {
     val program = Seq(3,3,1105,-1,9,1101,0,0,12,4,12,99,1)
-    new IntCodeComputer(program).run( 0) should be (Seq(0))
-    new IntCodeComputer(program).run( 10) should be (Seq(1))
-    new IntCodeComputer(program).run( -10) should be (Seq(1))
+    new IntCodeComputer(program).start( 0).output should be (Seq(0))
+    new IntCodeComputer(program).start( 10).output should be (Seq(1))
+    new IntCodeComputer(program).start( -10).output should be (Seq(1))
   }
 
   "(from day5) larger example" should "output 999 if the input value is below 8, output 1000 if the input value is equal to 8, or output 1001 if the input value is greater than 8." in {
@@ -89,38 +89,75 @@ class Day7PartSpec extends FlatSpec with Matchers {
       1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
       999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99)
 
-    new IntCodeComputer(program).run( 0) should be(Seq(999))
-    new IntCodeComputer(program).run( -890) should be(Seq(999))
-    new IntCodeComputer(program).run( 7) should be(Seq(999))
+    new IntCodeComputer(program).start( 0).output should be(Seq(999))
+    new IntCodeComputer(program).start( -890).output should be(Seq(999))
+    new IntCodeComputer(program).start( 7).output should be(Seq(999))
 
-    new IntCodeComputer(program).run( 8) should be(Seq(1000))
+    new IntCodeComputer(program).start( 8).output should be(Seq(1000))
 
-    new IntCodeComputer(program).run( 9) should be(Seq(1001))
-    new IntCodeComputer(program).run( 19) should be(Seq(1001))
+    new IntCodeComputer(program).start( 9).output should be(Seq(1001))
+    new IntCodeComputer(program).start( 19).output should be(Seq(1001))
   }
 
   "1rst example phase execution" should "yield the specified power" in {
-    Day7Part.chainAmplifiers(Seq(3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0), Seq(4,3,2,1,0), 0) should be (43210)
+    Day7Part1.chainAmplifiers(
+      Seq(3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0),
+      Seq(4,3,2,1,0)) should be (43210)
   }
 
   "1rst optimal phase settings of 1rst example" should "match" in {
-    Day7Part.optimalPhases(Seq(3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0)) should be (Seq(4,3,2,1,0), 43210)
+    Day7Part1.optimalPhases(Seq(3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0)) should be (Seq(4,3,2,1,0), 43210)
   }
 
   "2nd example phase execution" should "yield the specified power" in {
-    Day7Part.chainAmplifiers(Seq(3,23,3,24,1002,24,10,24,1002,23,-1,23, 101,5,23,23,1,24,23,23,4,23,99,0,0), Seq(0,1,2,3,4), 0) should be (54321)
+    Day7Part1.chainAmplifiers(
+      Seq(3,23,3,24,1002,24,10,24,1002,23,-1,23, 101,5,23,23,1,24,23,23,4,23,99,0,0),
+      Seq(0,1,2,3,4)) should be (54321)
   }
 
   "optimal phase settings of 2nd example" should "match" in {
-    Day7Part.optimalPhases(Seq(3,23,3,24,1002,24,10,24,1002,23,-1,23, 101,5,23,23,1,24,23,23,4,23,99,0,0)) should be (Seq(0,1,2,3,4), 54321)
+    Day7Part1.optimalPhases(
+      Seq(3,23,3,24,1002,24,10,24,1002,23,-1,23, 101,5,23,23,1,24,23,23,4,23,99,0,0)) should be (Seq(0,1,2,3,4), 54321)
   }
 
   "3rd example phase execution" should "yield the specified power" in {
-    Day7Part.chainAmplifiers(Seq(3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33, 1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0), Seq(1,0,4,3,2), 0) should be (65210)
+    Day7Part1.chainAmplifiers(
+      Seq(3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33, 1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0),
+      Seq(1,0,4,3,2)) should be (65210)
   }
 
   "optimal phase settings of 3nd example" should "match" in {
-    Day7Part.optimalPhases(Seq(3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33, 1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0)) should be (Seq(1,0,4,3,2), 65210)
+    Day7Part1.optimalPhases(
+      Seq(3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33, 1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0)
+    ) should be (Seq(1,0,4,3,2), 65210)
+  }
+
+  "part 2 1rst example chain" should "yield the specified power" in {
+    Day7Part2.loopingAmplifiersChain(
+      Seq(3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26, 27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5),
+      Seq(9,8,7,6,5)) should be (139629729)
+  }
+
+  "optimal phase settings of part 2 1rst example" should "match" in {
+    Day7Part2.optimalPhases(
+      Seq(3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26, 27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5)
+    ) should be (Seq(9,8,7,6,5), 139629729)
+  }
+
+  "part 2 2nd example chain" should "yield the specified power" in {
+    Day7Part2.loopingAmplifiersChain(
+      Seq(3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+        -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+        53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10),
+      Seq(9,7,8,5,6)) should be (18216)
+  }
+
+  "optimal phase settings of part 2 2nd example" should "match" in {
+    Day7Part2.optimalPhases(
+      Seq(3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+        -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+        53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10)
+    ) should be (Seq(9,7,8,5,6), 18216)
   }
 
 }
