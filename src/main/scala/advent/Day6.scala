5ec0e0fc88ec02e6b8922e80e4d32820c8a04816
sv3ndk <svend.vanderveken@gmail.com>
2019-12-09 11:54:37 +0100

    solution to day 6 part 2

--- src/main/scala/advent/Day6.scala
+++ src/main/scala/advent/Day6.scala
@@ -4,11 +4,35 @@ import scala.annotation.tailrec
 import scala.io.Source
 import scala.util.Using
 
-object Day6Part1 extends App {
+object Day6Parts extends App {
   Using(Source.fromFile("src/main/data/day6-orbits.txt")) {
     file =>
-      val sumOrbitCount = Day6.orbitCount(file.getLines().toSeq)
-      println(s"Advent of code 2019 - Day 6 / part 1: orbit count: $sumOrbitCount")
+      val sumOrbitCount = solvePart1(file.getLines().toSeq)
+      println(s"Advent of code 2019 - Day 6 / part 1: orbit count: $sumOrbitCount") // 294191
+  }
+
+  Using(Source.fromFile("src/main/data/day6-orbits.txt")) {
+    file =>
+      val orbitalTransfers = solvePart2(file.getLines().toSeq)
+      println(s"Advent of code 2019 - Day 6 / part 2: orbit transfers: $orbitalTransfers") //
+  }
+
+  def solvePart1(fileContent: Seq[String]) = {
+    val childToParentTree = Day6.childToParentTree(fileContent)
+    Day6.orbitCount(childToParentTree)
+  }
+
+  def solvePart2(fileContent: Seq[String]) = {
+    val childToParentTree = Day6.childToParentTree(fileContent)
+    val path1 = Day6.pathToRoot(childToParentTree, "SAN")
+    val path2 = Day6.pathToRoot(childToParentTree, "YOU")
+
+    val commonAncestor = Day6.deepestCommonAncestor(childToParentTree, "SAN", "YOU")
+
+    val hops1 = path1.takeWhile(_ != commonAncestor).size - 1
+    val hops2 = path2.takeWhile(_ != commonAncestor).size - 1
+
+    hops1 + hops2
   }
 }
 
@@ -19,22 +43,42 @@ object Day6 {
     (parentId.trim, childId.trim)
   }
 
-  def orbitCount(rawOrbits: Seq[String]) = {
-
-    // map providing the parent of every child
-    val parents = rawOrbits
+  def childToParentTree(rawOrbits: Seq[String]): Map[String, String] =
+    rawOrbits
       .map(parseOrbit)
       .map { case (parentId, childId) => (childId -> parentId) }
       .toMap
 
+  def isRoot(node: String) = node == "COM"
+
+  // finds the sequence of node from a the root to a child node
+  def pathToRoot(childToParentTree: Map[String, String], node: String): Seq[String] =
+    node +: (if (isRoot(node))
+      Nil
+    else pathToRoot(childToParentTree, childToParentTree(node)))
+
+  def pathFromRoot(childToParentTree: Map[String, String], node: String): Seq[String] =
+    pathToRoot(childToParentTree, node).reverse
+
+  def deepestCommonAncestor(childToParentTree: Map[String, String], node1: String, node2: String) = {
+    pathFromRoot(childToParentTree, node1)
+      .zip(pathFromRoot(childToParentTree, node2))
+      .takeWhile{ case(n1, n2) =>  n1 == n2 }
+      .last
+      ._1
+  }
+
+  // number of direct and indirect orbits around that node
+  def orbitCount(childToParentTree: Map[String, String]) = {
+
     @tailrec
     def orbitCount(node: String, count: Int): Int = {
-      if (node == "COM") count
-      else orbitCount(parents(node), count + 1)
+      if (isRoot(node)) count
+      else orbitCount(childToParentTree(node), count + 1)
     }
 
     // the set of all children is the set of all node (except "COM")
-    val allNodes = parents.keys
+    val allNodes = childToParentTree.keys
 
     allNodes.toSeq.map(orbitCount(_, 0)).sum
   }
--- src/test/scala/advent/Day6Spec.scala
+++ src/test/scala/advent/Day6Spec.scala
@@ -4,14 +4,69 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Day6Spec extends FlatSpec with Matchers {
 
+  //         G - H       J - K - L
+  //       /           /
+  //COM - B - C - D - E - F
+  //               \
+  //                I
+
   "example tree" should "be parsed correctly"in {
     val raw = Seq("COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L")
-    Day6.orbitCount(raw) should be (42)
+    Day6Parts.solvePart1(raw) should be (42)
   }
 
   "another example tree" should "be ok too " in {
     val raw = Seq("G)H", "B)C", "C)D", "D)E", "E)F", "COM)B", "B)G", "D)I", "E)J", "J)K", "K)L", "I)M")
-    Day6.orbitCount(raw) should be (47)
+    Day6Parts.solvePart1(raw) should be (47)
+  }
+
+  "finding path to root" should "be be correct"in {
+    val raw = Seq("COM" + ")B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L")
+    val childToParentTree = Day6.childToParentTree(raw)
+    Day6.pathFromRoot(childToParentTree, "B") should be ("COM" +: "B" +: Nil)
+    Day6.pathFromRoot(childToParentTree, "D") should be ("COM" +: "B" +: "C" +: "D" +: Nil)
+    Day6.pathFromRoot(childToParentTree, "L") should be ("COM" +: "B" +: "C" +: "D" +: "E" +: "J" +: "K" +: "L" +: Nil)
+  }
+
+  "finding the deepest common ancestor" should " be correct" in {
+    val raw = Seq("COM" + ")B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L")
+    val childToParentTree = Day6.childToParentTree(raw)
+
+    Day6.deepestCommonAncestor(childToParentTree, "D", "H") should be ("B")
+    Day6.deepestCommonAncestor(childToParentTree, "L", "H") should be ("B")
+    Day6.deepestCommonAncestor(childToParentTree, "J", "I") should be ("D")
+    Day6.deepestCommonAncestor(childToParentTree, "K", "F") should be ("E")
+    Day6.deepestCommonAncestor(childToParentTree, "C", "G") should be ("B")
+  }
+
+  //                           YOU
+  //                         /
+  //        G - H       J - K - L
+  //       /           /
+  //COM - B - C - D - E - F
+  //               \
+  //                I - SAN
+
+  "orbital transfers" should "be correct" in {
+
+    val raw = Seq("COM)B",
+    "B)C",
+    "C)D",
+    "D)E",
+    "E)F",
+    "B)G",
+    "G)H",
+    "D)I",
+    "E)J",
+    "J)K",
+    "K)L",
+    "K)YOU",
+    "I)SAN")
+
+    val childToParentTree = Day6.childToParentTree(raw)
+
+    Day6Parts.solvePart2(raw) should be (4)
   }
 
 }
+
